2efe21d80b428690c0d32a5413c561822705685a
I-keep-trying <26046173+I-keep-trying@users.noreply.github.com>
2019-08-16 15:36:03 -0400

    upgraded versions

--- README.md
+++ README.md
@@ -19,15 +19,10 @@
 
 2. **Some security notes**
 
-  API keys should never be pulished on your repo. So, I made a few changes to the way `.env` was handled - Gatsby does't exactly spell it out step by step anywhere so HERE YOU GO! 
+  API keys should never be pulished on your repo. So, I made a few notes about the way tokens should be handled, and how to do it specifically FOR GATSBY - Gatsby does't exactly spell it out step by step anywhere - it took a few tries for me so HERE YOU GO! 
 
-   Install dotenv
 
-   `
-   npm i dotenv
-   `
-
-3. **Already done in this repo. Invoke dotenv at the top of `gatsby-config.js` with this:**
+**Already done in this repo. Invoke dotenv at the top of `gatsby-config.js` with this:**
 
    ```
    require('dotenv').config({
@@ -35,18 +30,11 @@
    })
    ```
 
-4. **Be sure to prefix your variables in your `.env` files with `GATSBY_` or they will not be readable.**
+3. **Be sure to prefix your variables in your `.env` files with `GATSBY_` or they will not be readable.**
 
    ```
    GATSBY_SHOP_NAME=graphql
-   GATSBY_SHOPIFY_ACCESS_TOKEN=dd4d4dc14654not2breala7763305d71d1b3d38
+   GATSBY_SHOPIFY_ACCESS_TOKEN=fakedd4d4dc14654not2breala7763305d71d1b3d38token
    ```
 
-5. **Update your `package.json` scripts like this:**
-
-   ```
-   "build": "GATSBY_ENV=production gatsby build",
-   "develop": "GATSBY_ENV=development gatsby develop",
-   ```
-
-6. **Don't forget to add your `.env*` files to `.gitignore` before initial commit.**
+4. **Don't forget to add your `.env*` files to `.gitignore` before initial commit.**
--- gatsby-config.js
+++ gatsby-config.js
@@ -5,8 +5,8 @@ require('dotenv').config({
 module.exports = {
   siteMetadata: {
     title: `Gatsby Shopify Starter`,
-    description: `Kick off your next, ecommerce experience with this Gatsby starter. This starter ships with credentials to a shopify demo store so you can try it out immediately.`,
-    author: `@alexanderhorl`,
+    description: `Description goes here.`,
+    author: `@Andrea_Crego`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -29,29 +29,14 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`, 
       },
     },
     {
-      resolve: `gatsby-source-shopify2`,
+      resolve: `gatsby-source-shopify`,
       options: {
-        // The domain name of your Shopify shop. This is required.
-        // Example: 'gatsby-source-shopify-test-shop' if your Shopify address is
-        // 'gatsby-source-shopify-test-shop.myshopify.com'.
         shopName: process.env.GATSBY_SHOP_NAME,
-
-        // An API access token to your Shopify shop. This is required.
-        // You can generate an access token in the "Manage private apps" section
-        // of your shop's Apps settings. In the Storefront API section, be sure
-        // to select "Allow this app to access your storefront data using the
-        // Storefront API".
-        // See: https://help.shopify.com/api/custom-storefronts/storefront-api/getting-started#authentication
         accessToken: process.env.GATSBY_SHOPIFY_ACCESS_TOKEN,
-
-        // Set verbose to true to display a verbose output on `npm run develop`
-        // or `npm run build`. This prints which nodes are being fetched and how
-        // much time was required to fetch and process the data.
-        // Defaults to true.
         verbose: true,
       },
     },
@@ -63,8 +48,6 @@ module.exports = {
         respectDNT: true,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
   ],
 }
